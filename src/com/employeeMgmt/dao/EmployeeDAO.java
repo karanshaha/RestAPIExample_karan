edcfe21c8a50c989d9da5919a203d582e98266de
Karan Shaha <karans@synerzipune.local>
2016-04-19 15:49:03 +0530

    Changed the location of war file

--- src/com/employeeMgmt/dao/EmployeeDAO.java
+++ src/com/employeeMgmt/dao/EmployeeDAO.java
@@ -8,7 +8,7 @@ public class EmployeeDAO {
 	{
 		Employee emp = new Employee();
 		emp.setDateOfJoining("01-02-2001");
-		emp.setDepartment("Sales");
+		emp.setDepartment("Computer");
 		emp.setEmail("test@example.com");
 		emp.setEmployeeId("1001");
 		emp.setFirstName("John");
