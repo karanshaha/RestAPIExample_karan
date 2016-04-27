a35eab6a6d613862575e3cc5ed6a89205a3a1696
Karan Shaha <karans@synerzipune.local>
2016-04-27 14:34:42 +0530

    This is test

---
+++ src/com/employeeMgmt/constants/HTTPHeaderNames.java
@@ -0,0 +1,6 @@
+package com.employeeMgmt.constants;
+
+public interface HTTPHeaderNames {
+	public static final String SERVICE_KEY = "service_key";
+    public static final String AUTH_TOKEN = "auth_token";
+}
